ffdb33f8dae29eb6bbc7fc8c35d6b049864c22d3
jonjab <jonjab@ucsb.edu>
2025-07-16 13:51:33 -0700

    changes made while teaching tuesday

--- ep04.R
+++ ep04.R
@@ -13,7 +13,13 @@ library(dplyr)
 DTM_HARV <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_dtmCrop.tif")
 DSM_HARV <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
 
+describe(DTM_HARV)
+describe(DSM_HARV)
+
 summary(DTM_HARV)
+summary(DSM_HARV)
+
+crs(DTM_HARV) == crs(DSM_HARV)
 
 # make dataframes:
 DSM_HARV_df <- as.data.frame(DSM_HARV, xy=TRUE)
@@ -70,15 +76,53 @@ ggplot(CHM_HARV_df) +
 # 1 What is the min and maximum value for the Harvard Forest 
 #   Canopy Height Model (CHM_HARV) that we just created?
 
+summary(CHM_HARV_df)
+# min = 0
+# max = 38.18
+
 #  2 What are two ways you can check this range of data for CHM_HARV?
 
+# summary or stdout
+
 #  3 What is the distribution of all the pixel values in the CHM?
+
+# histograms show distributions.
 
 #  4 Plot a histogram with 6 bins instead of the default 
 #    and change the color of the histogram.
 
+ggplot(CHM_HARV_df) +
+  geom_histogram(aes(HARV_dsmCrop), bins = 6)
+
 #  5 Plot the CHM_HARV raster using breaks that make sense for the data. 
 #    Include an appropriate color palette for the data, plot title and no axes ticks / labels.
+
+ggplot() +
+  geom_raster(data = CHM_HARV_df, 
+              aes(x = x, y = y, fill = HARV_dsmCrop)) +
+  scale_fill_gradientn(name = "Canopy Height", colors = terrain.colors(3)) + 
+  ggtitle("What makes sense?") +
+    coord_quickmap()
+
+my_scale <- c(0, 2, 10, 35)    
+
+# like this!
+# DSM_HARV_df <- DSM_HARV_df %>% 
+#   mutate(fct_elevation6 = cut(HARV_dsmCrop, breaks=6))
+
+CHM_HARV_df <- CHM_HARV_df %>% 
+  mutate(classes = cut(HARV_dsmCrop, breaks=my_scale))
+
+str(CHM_HARV_df)
+summary(CHM_HARV_df)
+
+ggplot() +
+  geom_raster(data = CHM_HARV_df, 
+              aes(x = x, y = y, fill = classes)) +
+  scale_fill_manual(values=terrain.colors(3)) +
+  ggtitle("What makes sense? Does 3 make sense?") +
+coord_quickmap()
+
 
 
 # Efficient Raster Calculations
@@ -87,7 +131,10 @@ ggplot(CHM_HARV_df) +
 
 # Export a geotiff
 #
-
+writeRaster(CHM_HARV, "CHM_HARV.tiff",
+            filetype="GTIFF",
+            overwrite=TRUE,
+            NAflag=-9999)
 
 # final challenge: do it all again in the desert.
 
--- ep05.R
+++ ep05.R
@@ -5,8 +5,11 @@ rm(list=ls())
 current_episode <- 5
 
 library(terra)
-library(ggplot2)
 library(dplyr)
+
+# library(ggplot2)
+# we can leave ggplot out this time.
+
 
 
 # the lesson leaves out how to check for the number of layers:
@@ -30,7 +33,7 @@ dev.off()
 # Challenge
 # discuss: is this gratuitous? have
 # we had enough of summary() and standard-out?
-
+describe("data/NEON-DS-Airborne-Remote-Sensing/HARV/RGB_Imagery/HARV_RGB_Ortho.tif")
 
 
 
@@ -38,7 +41,7 @@ dev.off()
 # Import A Specific Band
 RGB_band2_HARV <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/RGB_Imagery/HARV_RGB_Ortho.tif", lyr=2)
 plot(RGB_band2_HARV)
-
+summary(RGB_band1_HARV)
 
 # Challenge: Making Sense of Single Band Images
 # the lesson wants us to compare these visually.
@@ -50,6 +53,9 @@ plot(RGB_band2_HARV)
 # if we don't specify the layer, rast()
 # loads all of them into a stack:
 RGB_stack_HARV <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/RGB_Imagery/HARV_RGB_Ortho.tif")
+str(RGB_stack_HARV)
+nlyr(RGB_stack_HARV)
+
 
 # the lesson buries the lead. let's plot it:
 plot(RGB_stack_HARV)
@@ -68,6 +74,7 @@ RGB_stack_HARV[[2]]
 # Create A Three Band Image
 plotRGB(RGB_stack_HARV, r=1, g=2, b=3)
 
+plotRGB(RGB_stack_HARV, r=3, g=2, b=1)
 
 
 
@@ -84,6 +91,9 @@ plotRGB(RGB_stack_HARV, r=1, g=2, b=3,
 # but we don't really know what it's doing.
 methods(class=class(RGB_stack_HARV))
 
+?scale
+
+
 # Challenge: nodata values.
 # Let’s explore what happens with NoData values when working 
 # with RasterStack objects and using the plotRGB() function. 
@@ -94,21 +104,29 @@ methods(class=class(RGB_stack_HARV))
 #
 #  View the files attributes. Are there NoData values assigned for this file?
 
+describe("data/NEON-DS-Airborne-Remote-Sensing/HARV/RGB_Imagery/HARV_Ortho_wNA.tif")
+
 #  If so, what is the NoData Value?
 
 #  How many bands does it have?
+HARV_NA <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/RGB_Imagery/HARV_Ortho_wNA.tif")
+nlyr(HARV_NA)
 
 #  Load the multi-band raster file into R.
 
 #  Plot the object as a true color image.
+plotRGB(HARV_NA, r=1, g=2, b=3)
 
 #  What happened to the black edges in the data?
 
 #  What does this tell us about the difference in the data structure 
 #  between HARV_Ortho_wNA.tif and HARV_RGB_Ortho.tif (R object RGB_stack). 
+HARV_NA
+RGB_stack_HARV
 
 #  How can you check?
-
+describe("data/NEON-DS-Airborne-Remote-Sensing/HARV/RGB_Imagery/HARV_Ortho_wNA.tif")
+describe(RGB_stack_HARV)
 
 # SpatRaster in R
 str(RGB_stack_HARV)
@@ -144,8 +162,8 @@ RGB_sds_HARV[[2]]
 #  What methods can be used on the RGB_stack_HARV object?
 #  What methods can be used on a single band within RGB_stack_HARV?
 
-
-
+methods(class=class(RGB_sds_HARV))
+methods(class=class(RGB_stack_HARV))
 
 
 
