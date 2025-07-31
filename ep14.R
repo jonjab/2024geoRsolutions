707abc39a9ffaf48f0f062386976d9fed4507f96
jonjab <jonjab@ucsb.edu>
2025-07-31 08:47:52 -0700

    ep 14 teaching prep

--- ep14.R
+++ ep14.R
@@ -1,82 +1,104 @@
 # ep 14
 # Deriving values from raster time series
 
-# extracting pixels from raters, saving summary values to a csv file 
+# extracting pixels from rasters, saving summary values to a csv file 
 # plotting pixel summary values using ggplot() 
 # comparing NDVI values between two different sites 
 
 library(pacman)
 pacman::p_unload(pacman::p_loaded(), character.only = TRUE)
 
+library(dplyr)
+library(terra)
+library(ggplot2)
+library(tidyr)
+
+
 current_episode <- 14
 
+# objects from last time
+NDVI_HARV_path <- "data/NEON-DS-Landsat-NDVI/HARV/2011/NDVI"
+all_NDVI_HARV <- list.files(NDVI_HARV_path,
+                            full.names = TRUE,
+                            pattern = ".tif$")
 
+NDVI_HARV_stack <- rast(all_NDVI_HARV)
+head(NDVI_HARV_stack)
+NDVI_HARV_stack <- NDVI_HARV_stack / 10000
+
+# episode starts here.
+
+# take the average of all the pixels
+# in each raster (note we are looking at the spatraster object, not the dataframe)
 avg_NDVI_HARV <- global(NDVI_HARV_stack, mean)
-avg_NDVI_HARV
 
+avg_NDVI_HARV
 head(avg_NDVI_HARV)
 
-#clean up column names and change the name 
+
+# change the column name
 names(avg_NDVI_HARV) <- "meanNDVI"
 head(avg_NDVI_HARV)
 
-# do it again for site and year
+# add columns for site and year 
 avg_NDVI_HARV$site <- "HARV"
 avg_NDVI_HARV$year <- "2011"
 
 #voila
 head(avg_NDVI_HARV)
 
-#Julian Days 
+# Extract Julian Dates from row names 
+# that's a 'bar' or a pipe. it is 'or'
+# replace x OR '_HARV_ndvi_crop' with nothing
+# gsub is 1st or all occurences.
+
 julianDays <- gsub("X|_HARV_ndvi_crop", "", row.names(avg_NDVI_HARV))
 julianDays
 
 # add julian days as a column
 avg_NDVI_HARV$julianDay <- julianDays
+
+# but it's text. we need dates.
 class(avg_NDVI_HARV$julianDay)
 
-#converting julian dayes to date class
+# converting julian day to date class
+# there's a NEON tutorial for this!
 
-
+# what's the first day of the year?
 origin <- as.Date("2011-01-01")
 
+# convert julian day to integer
 avg_NDVI_HARV$julianDay <- as.integer(avg_NDVI_HARV$julianDay)
 
-avg_NDVI_HARV$Date<- origin + (avg_NDVI_HARV$julianDay - 1)
+# the -1 is to start counting on Jan 1 instead of Jan 0.
+avg_NDVI_HARV$Date <- origin + (avg_NDVI_HARV$julianDay - 1)
 head(avg_NDVI_HARV$Date)
 
 class(avg_NDVI_HARV$Date)
 
-#challenge: NDVI for SJER/San Joaquin 
-#will need this to compare two sites later
 
+
+# challenge: NDVI for SJER/San Joaquin 
+# will compare the two sites later
+
+# Create a dataframe containing the mean NDVI values 
+# and the Julian days that the data was collected (in date format) 
+# for the NEON San Joaquin Experimental Range field site. NDVI data for SJER are located in the NEON-DS-Landsat-NDVI/SJER/2011/NDVI directory.
+
+# here's the first part:
 NDVI_path_SJER <- "data/NEON-DS-Landsat-NDVI/SJER/2011/NDVI"
 
 all_NDVI_SJER <- list.files(NDVI_path_SJER,
                             full.names = TRUE,
                             pattern = ".tif$")
 
-NDVI_stack_SJER <- rast(all_NDVI_SJER)
-names(NDVI_stack_SJER) <- paste0("X", names(NDVI_stack_SJER))
 
-NDVI_stack_SJER <- NDVI_stack_SJER/10000
 
-#mean values for each day > dataframe it
-avg_NDVI_SJER <- as.data.frame(global(NDVI_stack_SJER, mean))
 
-names(avg_NDVI_SJER) <- "meanNDVI"
-avg_NDVI_SJER$site <- "SJER"
-avg_NDVI_SJER$year <- "2011"
 
-julianDays_SJER <- gsub("X|_SJER_ndvi_crop", "", row.names(avg_NDVI_SJER))
-origin <- as.Date("2011-01-01")
-avg_NDVI_SJER$julianDay <- as.integer(julianDays_SJER)
 
-avg_NDVI_SJER$Date <- origin + (avg_NDVI_SJER$julianDay - 1)
 
-head(avg_NDVI_SJER)
-
-#plot NDVI using ggplot
+# plot NDVI using ggplot
 
 ggplot(avg_NDVI_HARV, aes(julianDay, meanNDVI)) +
   geom_point() +
@@ -84,11 +106,20 @@ ggplot(avg_NDVI_HARV, aes(julianDay, meanNDVI)) +
           subtitle = "NEON Harvard Forest Field Site") +
   xlab("Julian Days") + ylab("Mean NDVI")
 
-#challenge plot SJER/San Joaquin 
+
+
+
+# challenge ################
+# plot SJER/San Joaquin 
+# don't use black
+
 ggplot(avg_NDVI_SJER, aes(julianDay, meanNDVI)) +
   geom_point(colour = "SpringGreen4") +
   ggtitle("Landsat Derived NDVI - 2011", subtitle = "NEON SJER Field Site") +
   xlab("Julian Day") + ylab("Mean NDVI")
+
+
+
 
 
 #comparing the NDVI of two sites on one plot 
@@ -97,6 +128,8 @@ ggplot(avg_NDVI_SJER, aes(julianDay, meanNDVI)) +
 # same # of columns with same column names 
 NDVI_HARV_SJER <- rbind(avg_NDVI_HARV, avg_NDVI_SJER)
 
+str(NDVI_HARV_SJER)
+
 ggplot(NDVI_HARV_SJER, aes(x = julianDay, y = meanNDVI, colour = site)) +
   geom_point(aes(group = site)) +
   geom_line(aes(group = site)) +
@@ -104,8 +137,8 @@ ggplot(NDVI_HARV_SJER, aes(x = julianDay, y = meanNDVI, colour = site)) +
           subtitle = "Harvard Forest vs San Joaquin") +
   xlab("Julian Day") + ylab("Mean NDVI")
 
-# I dont like julian days, can I get it like a normal person?
 
+# I dont like julian days, can I get it like a normal person?
 ggplot(NDVI_HARV_SJER, aes(x = Date, y = meanNDVI, colour = site)) +
   geom_point(aes(group = site)) +
   geom_line(aes(group = site)) +
@@ -123,6 +156,12 @@ ggplot(avg_NDVI_HARV_clean, aes(x = julianDay, y = meanNDVI)) +
           subtitle = "NEON Harvard Forest Field Site") +
   xlab("Julian Days") + ylab("Mean NDVI")
 head(avg_NDVI_HARV_clean)
+
+
+# final challenge:
+# remove outliers from SJER
+# and recreate the plot of both of them.
+
 
 
 #remove row names
